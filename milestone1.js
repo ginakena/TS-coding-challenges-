a47f858466e69620c0ee598c23d637e86bfc9388
ginakena <makenareginam@gmail.com>
2025-06-01 09:20:50 +0300

    Challenge 2: converting minutes to seconds

--- milestone1.js
+++ milestone1.js
@@ -2,5 +2,10 @@
 function addition(num1, num2) {
     return num1 + num2;
 }
-console.log(addition(3, 5));
-console.log(addition(-6, 9));
+//console.log(addition(3, 5));
+//console.log(addition(-6, 9));
+//convert minutes to seconds
+function convert(int) {
+    return int * 60;
+}
+console.log(convert(5));
--- milestone1.ts
+++ milestone1.ts
@@ -2,5 +2,11 @@
 function addition(num1: number, num2: number): number {
     return num1 + num2; 
 }
-console.log(addition(3, 5));
-console.log(addition(-6, 9));
+//console.log(addition(3, 5));
+//console.log(addition(-6, 9));
+
+//convert minutes to seconds
+function convert(int: number): number {
+    return int * 60;
+}
+console.log(convert(5));
